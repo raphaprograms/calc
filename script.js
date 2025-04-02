27aa8e0ce80ecca2066d4a61bff5751dc90df3d2
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-04-02 16:11:51 -0700

    Fix link between html/css and add operator btns

--- script.js
+++ script.js
@@ -51,23 +51,31 @@ display.appendChild(input);
 container.appendChild(display);
 
 const tools = document.createElement('div');
-const clear = document.createElement('button');
-clear.textContent = 'Clear';
-tools.appendChild(clear);
+
 
 const buttons = []
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i <= 15; i++) {
     button = document.createElement('button');
     button.textContent = `${i}`;
-    if (i == 10) {
+     if (i == 0) {
+        button.textContent = `C`;
+     } else if (i == 10) {
         button.textContent = `0`; 
+    } else if (i == 11) {
+        button.textContent = `+`; 
+    } else if (i == 12) {
+        button.textContent = `-`; 
+    } else if (i == 13) {
+        button.textContent = `*`; 
+    } else if (i == 14) {
+        button.textContent = `/`; 
+    } else if (i == 15) {
+        button.textContent = `=`; 
     }
+
+
     buttons.push(button);
 }
 
 tools.append(...buttons);
 container.appendChild(tools);
-
-const equals = document.createElement('button');
-equals.textContent = '=';
-tools.appendChild(equals);
